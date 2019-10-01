8a221298ab1d7b43813963b79a1dfa0999471e02
Gabo925 <54083944+Gabo925@users.noreply.github.com>
2019-10-01 17:43:55 -0500

    Version final

--- Coladera.cpp
+++ Coladera.cpp
@@ -1,1 +1,46 @@
 #include <iostream>
+using namespace std;
+
+void primo(int v[],int n);
+
+int main()
+{
+	int v[1000];
+	
+	primo(v,1000);
+	
+}
+
+void primo(int v[],int n)
+{
+	int esp, cont, cont2,t=1;
+	for(int i=0;i<n;i++)
+{
+	v[i]=true;	
+}
+
+	for(int i=2;i<n;i++)
+	{
+		for(int j=2;i*j<n;j++)
+		{
+			v[i*j]=false;
+		}
+	}
+	esp = 10;
+    cont = -1;
+    cont2 = 10;
+    for (int i=0;i<n;i++)
+	{
+        cont++;
+        if(v[i])
+		{
+		cout<<"|"<< i;	
+		}else
+		{
+		cout <<"|*";
+		}
+        if(v[i]!=t);
+          if(cont/esp*cont2==10);
+                cont2=cont2+10;
+   }
+}
